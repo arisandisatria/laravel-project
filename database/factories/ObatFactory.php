2a0cf17eb9194b769f7ef3d8fbcabd61d9470996
Airtas <89629005+ArisandiSatria@users.noreply.github.com>
2026-04-26 01:56:46 +0700

    fix: obatFactory

--- database/factories/ObatFactory.php
+++ database/factories/ObatFactory.php
@@ -15,15 +15,16 @@ class ObatFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-{
-    return [
-        'kode_obat' => $this->faker->unique()->numerify('OBT-####'),
-        'nama_obat' => $this->faker->word() . ' 500mg',
-        'kategori' => $this->faker->randomElement(['Antibiotik', 'Analgetik', 'Vitamin']),
-        'satuan' => $this->faker->randomElement(['Tablet', 'Botol', 'Kapsul']),
-        'stok' => $this->faker->numberBetween(10, 200),
-        'harga' => $this->faker->numberBetween(2, 50) * 1000,
-    ];
-}
+   public function definition(): array
+    {
+        return [
+            // Ganti $this->faker menjadi fake()
+            'kode_obat' => fake()->unique()->numerify('OBT-####'),
+            'nama_obat' => fake()->word() . ' 500mg',
+            'kategori'  => fake()->randomElement(['Antibiotik', 'Analgetik', 'Vitamin']),
+            'satuan'    => fake()->randomElement(['Tablet', 'Botol', 'Kapsul']),
+            'stok'      => fake()->numberBetween(10, 200),
+            'harga'     => fake()->numberBetween(2, 50) * 1000,
+        ];
+    }
 }
